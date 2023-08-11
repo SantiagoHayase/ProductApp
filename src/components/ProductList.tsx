f2bdfcf5d7b89c0da6145db9e9aeb923d55eef7c
Santiago Hayase <santiagohayase@gmail.com>
2023-08-11 01:15:39 -0300

    push

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -19,6 +19,7 @@ export default async function ProductList() {
     <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
       {products.map((product: any) => (
         <ProductCard
+          key={product._id}
           id={product._id}
           name={product.name}
           price={product.price}
